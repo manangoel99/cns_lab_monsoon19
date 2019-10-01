08d35e445eb3bcf421ed831b9295089d6f8d7e42
yp201 <yashaswi.pathak201@gmail.com>
2019-10-01 14:06:16 +0530

    Tutorial 1 added

---
+++ tutorial_1/README.md
@@ -0,0 +1,9 @@
+# Tutorial 1:
+## Deadline (5 PM, 1.10.2019)
+
+- Refer Course page on moodle for details regarding this assignment.
+- Use the given boilerpalte and preferably use libraries like numpy for a vectorized implementation.
+- Take mu and sigma as 1.
+- Appropriately select the volume of the box, delta time and time steps.
+- Periodic boundary conditions need not be considered.
+- Submit all the scripts and outputs in a zip file on moodle(yourrollno.zip)
---
+++ tutorial_1/tutorial_1.py
@@ -0,0 +1,67 @@
+
+class Simulator():
+
+	def __init__(self):
+		
+		pass
+
+	def setupSimulation(self):
+		'''
+		Setup the simulations 
+		'''
+		pass
+
+	def initializePostions(self):
+		'''
+		Initalize the positions. 
+		'''
+		pass
+
+	def initializeVelocities(self):
+		'''
+		Initalize the velocities. 
+		'''
+		pass
+
+	def doMultipleSteps(self):
+		'''
+		do the steps
+		'''
+		pass
+	
+	def eulerIntegrate(self):
+		'''
+		apply euler scheme
+		'''
+		pass
+
+	def velocityVerletIntegrate(self):
+		'''
+		apply velocity verlet integrator
+		'''
+		pass
+
+	def totalEnergy(self):
+		'''
+		calculate total energy
+		'''
+		pass
+
+	def potentialEnergy(self):
+		'''
+		calculate potential energy
+		'''
+		pass
+
+	def kineticEnergy(self):
+		'''
+		calculate kinetic energy
+		'''
+		pass
+
+
+#Initialize the simulator class
+
+System=Simulator()
+
+#Call the methods from the simulator class appropriately 
